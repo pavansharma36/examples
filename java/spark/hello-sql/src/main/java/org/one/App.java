7874a4da389466b3e403f0c58c183d54ae00e35d
Pavan Sharma <sharmapavan369@gmail.com>
2025-09-28 22:14:28 +0530

    try hive with spark

--- java/spark/hello-sql/src/main/java/org/one/App.java
+++ java/spark/hello-sql/src/main/java/org/one/App.java
@@ -1,97 +1,51 @@
 package org.one;
 
-import javax.xml.crypto.Data;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.functions;
 
 import static org.apache.spark.sql.functions.*;
+
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        SparkSession spark = SparkSession.builder()
-            .appName("test")
-            .config("spark.master", "local[5]")
-            .getOrCreate();
-
-        Dataset<Row> movies = movies(spark);
-
-        Dataset<Row> ratings = ratings(spark);
-
-        Dataset<Row> movieRatingCountDesc = ratings.groupBy("movieId")
-                .avg("rating");
-        movieRatingCountDesc.join(movies, movies.col("movieId").equalTo(movieRatingCountDesc.col("movieId")))
-                .sort(desc("avg(rating)"))
-                    .show(100);
+  public static void main(String[] args) {
+    SparkSession spark = SparkSession.builder().appName("test")
+        .config("spark.sql.warehouse.dir", "/user/hive/warehouse").enableHiveSupport()
+        .getOrCreate();
 
 
-        //movieRatingCountDesc.show();
+    spark.sql("SHOW DATABASES").show();
 
-//        movieRatingCountDesc.join(movies, movieRatingCountDesc.col("movieId")
-//            .equalTo(movies.col("movieId"))).sort(desc("count")).show();
+    spark.sql("SHOW TABLES FROM imdb").show();
 
-//        movies.createOrReplaceTempView("movies");
-//        ratings.createOrReplaceTempView("ratings");
-//
-//        Dataset<Row> movieRatingDesc = spark.sqlContext().sql("SELECT m.movieId, m.title, r.ratingCount FROM movies m " +
-//            "INNER JOIN (SELECT movieId, count(movieId) AS ratingCount FROM ratings GROUP BY movieId) r " +
-//            "ON m.movieId = r.movieId " +
-//            "ORDER BY r.ratingCount DESC");
-//
-//        movieRatingDesc.write()
-//            .option("header", "true")
-//            .csv("/home/pavan/Desktop/test_spark.csv");
+    Dataset<Row> ratings = spark.table("imdb.rating");
 
-        System.out.println(spark);
-    }
+    Dataset<Row> avgRatings = ratings.groupBy("movieId")
+        .agg(avg("rating").as("avg_rating"), count("movieId").as("total_rating"));
 
-    private static Dataset<Row> movies(SparkSession spark) {
-        StructType st = new StructType()
-            .add("movieId", "integer")
-            .add("title", "string")
-            .add("genres", "string");
+    Dataset<Row> movies = spark.table("imdb.movie");
 
-        return spark.read().option("header", "true")
-            .schema(st)
-            .csv("/home/pavan/Desktop/data/ml-25m/movies.csv");
-    }
+    avgRatings = avgRatings.join(movies, avgRatings.col("movieId").equalTo(movies.col("id")))
+        .withColumn("avg_by_count", col("avg_rating").multiply(col("total_rating")).$div(lit(5)))
+        .sort(col("avg_by_count").desc());
 
-    private static Dataset<Row> ratings(SparkSession spark) {
-        StructType st = new StructType()
-            .add("userId", "integer")
-            .add("movieId", "integer")
-            .add("rating", "float")
-            .add("timestamp", "long");
+    String path = "/user/pavan/ml-25m/top_ratings";
+    System.out.println("Writing to " + path);
+    avgRatings.write()
+        .mode("overwrite")
+        .format("parquet")
+        .option("path", path)
+        .saveAsTable("imdb.top_ratings");
 
-        return spark.read().option("header", "true")
-            .schema(st)
-            .csv("/home/pavan/Desktop/data/ml-25m/ratings.csv");
-    }
+    System.out.println(spark);
+  }
 
 
-//    StructType st = new StructType()
-//        .add("id", "integer")
-//        .add("firstname", "string")
-//        .add("lastname", "string")
-//        .add("email", "string")
-//        .add("profession", "string");
-//
-//    Dataset<Row> df = spark.read().option("header", "true")
-//        .schema(st)
-//        .csv("/home/pavan/Desktop/test.csv");
-//        df.show(10);
-//
-//        df.createOrReplaceTempView("test_view");
-//
-//        df.sqlContext().sql("select * from test_view where profession = 'developer'")
-//                .show();
-//
-//        spark.sqlContext().sql("select * from test_view where profession = 'developer'")
-//                .show();
-//        df.groupBy("profession").count().show();
 }
